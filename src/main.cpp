a17be9f42bedf8df8f7464f13453a35e8247361b
RhettTheRhett <93895330+RhettTheRhett@users.noreply.github.com>
2026-06-05 22:41:07 -0600

    milestone 2: added wasd movement, improved camera movement, vertical movement

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@ int main(){
 
 
     InitWindow(1080, 720, "Voxel Game");
-    SetTargetFPS(60);
+    SetTargetFPS(200);
 
     float yaw   = -90.0f; // start facing -Z (into the scene)
     float pitch = 0.0f;
     float speed = 5.0f;
     float sensitivity = 0.1f;
 
-     
+   
 
 
     Camera3D camera = {};
@@ -43,9 +43,25 @@ int main(){
         forward.y = sinf(pitchRad);
         forward.z = cosf(pitchRad) * sinf(yawRad);
 
-        camera.target = camera.position + forward;
         
 
+        Vector3 moveForward = {};
+        Vector3 moveSide = {};
+        
+        moveForward = {cosf(yawRad), 0, sinf(yawRad)};
+        moveSide = {cosf(yawRad + 90.0f * DEG2RAD), 0, sinf(yawRad + 90.0f * DEG2RAD)};
+
+        float deltaTime = GetFrameTime();
+
+        if(IsKeyDown(KEY_W)) camera.position += moveForward * speed * deltaTime;
+        if(IsKeyDown(KEY_S)) camera.position -= moveForward * speed * deltaTime;
+        if(IsKeyDown(KEY_A)) camera.position -= moveSide * speed * deltaTime;
+        if(IsKeyDown(KEY_D)) camera.position += moveSide * speed * deltaTime;
+
+        if(IsKeyDown(KEY_SPACE)) camera.position.y += speed * deltaTime;
+        if(IsKeyDown(KEY_LEFT_CONTROL)) camera.position.y -= speed * deltaTime;
+
+        camera.target = camera.position + forward;
         
         
         BeginDrawing();
@@ -58,6 +74,7 @@ int main(){
 
         DrawText(TextFormat("dx: %.2f  dy: %.2f", delta.x, delta.y), 10, 10, 20, BLACK);
         DrawText(TextFormat("pitch: %.2f yaw: %.2f", pitch, yaw), 10, 35, 20, BLACK);
+        DrawText(TextFormat("FPS: %d", GetFPS()), 10, 60, 20, BLACK);
 
         EndDrawing();
     }
